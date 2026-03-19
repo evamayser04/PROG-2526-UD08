cc69f1d2809c3558de9b6f93b7cad89804b01384
Eva <evamayser04@gmail.com>
2026-03-19 21:10:14 +0100

    COMITEADO

--- a01/DispositivoConectado.java
+++ a01/DispositivoConectado.java
@@ -1,5 +1,21 @@
 public interface DispositivoConectado {
     int WIFI = 7;
 
-    
+    void encender(){
+
+
+    }
+
+    void apagar(){
+
+        
+    }
+
+    static void conectar(){
+
+    }
+
+    static void mostrarInfoGeneral(){
+
+    }
 }
