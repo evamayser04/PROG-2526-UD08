329031184bcf725272701da832f974a45a205d3f
Eva <evamayser04@gmail.com>
2026-03-24 09:03:19 +0100

    COMITEADO

---
+++ a05/Main.java
@@ -0,0 +1,19 @@
+package a05;
+import a03.Notificable;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        String mensajeCEO = "Cuu cuu: El CEO os saluda"
+
+        Notificable paloma = new Notificable() {
+            @Override
+            public void enviarNotificacion(String mensaje){
+                System.out.println(mensaje);
+            }
+        };
+
+        paloma.enviarNotificacion(mensajeCEO);
+    }
+}
+
